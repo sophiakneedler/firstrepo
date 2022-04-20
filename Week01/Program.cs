1052adc7ca1e5288003b4c344ee33e9d75041fff
sophiakneedler <104103167+sophiakneedler@users.noreply.github.com>
2022-04-20 14:50:23 -0600

    Add files via upload

---
+++ Week01/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Hi friends!");
+Console.WriteLine("My favorite color is orange");
+Console.WriteLine("What is your favorite color?");
+string color = Console.ReadLine();
+Console.WriteLine($"Your favorite color is {color}");
+
+int x=400;
+Console.Write(x);
